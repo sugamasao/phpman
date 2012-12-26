e1696e367d19959f08ef366b08bcdc1c5b470e34
tokushima <rhaco1004@gmail.com>
2012-12-26 18:38:28 +0900

    bootstarp

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+spl_autoload_register(function($c){
+	$cp = str_replace('\\','//',(($c[0] == '\\') ? substr($c,1) : $c));
+	foreach(explode(PATH_SEPARATOR,get_include_path()) as $p){
+		if(($r = realpath($p)) !== false){
+			if(is_file($f=($r.'/'.$cp.'.php')) || is_file($f=($r.'/'.$cp.'/'.basename($cp).'.php'))){
+				require_once($f);
+				
+				if(class_exists($c,false) || interface_exists($c,false)){
+					if(method_exists($c,'__import__') && ($i = new ReflectionMethod($c,'__import__')) && $i->isStatic()) $c::__import__();
+					if(method_exists($c,'__shutdown__') && ($i = new ReflectionMethod($c,'__shutdown__')) && $i->isStatic()) register_shutdown_function(array($c,'__shutdown__'));
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+},true,false);
