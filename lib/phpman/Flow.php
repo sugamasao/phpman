c398ca429c6d1e040dfb799dc4dfa51e951c97c4
tokushima <rhaco1004@gmail.com>
2013-01-30 01:23:09 +0900

    展開

--- lib/phpman/Flow.php
+++ lib/phpman/Flow.php
@@ -17,12 +17,40 @@ class Flow{
 	}
 	public function read($map){
 		$pathinfo = preg_replace("/(.*?)\?.*/","\\1",(isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : null));
-		if(is_string($map) && preg_match('/^[\w\.]+$/',$map)) $map = array('patterns'=>array(''=>array('action'=>$map)));
-		if(!isset($map['patterns']) || is_array($map['patterns'])) throw new InvalidArgumentException('patterns not found');
-		foreach($map['patterns'] as $k => $v){
-			if(is_int($k) || isset($map['patterns'][$k]['patterns'])){
-				$map['patterns'][$k] = $this->map_pattern($map['patterns'][$k]);
+		if(is_string($map) && preg_match('/^[\w\.]+$/',$map)) $map = array('pattern'=>array(''=>array('action'=>$map)));
+		if(!isset($map['pattern']) || !is_array($map['pattern'])) throw new InvalidArgumentException('pattern not found');
+		foreach($map['pattern'] as $k => $v){
+			$maps_url = is_int($k) ? null : $k.'/';			
+			
+			if(is_int($k) || isset($map['pattern'][$k]['pattern'])){
+				foreach($map['pattern'][$k]['pattern'] as $pk => $pv){
+					$map['pattern'][$maps_url.$pk] = $this->map_pattern($pv);
+				}
+				unset($map['pattern'][$k]['pattern']);
+			}else{
+				$map['pattern'][$k] = $this->map_pattern($map['pattern'][$k]);
 			}
 		}
+		return $map;
+		
+		/***
+			$self = new self();
+			$map = $self->read(array(
+						'pattern'=>array(
+							'abc'=>array('action'=>'abc.def.Ghi')
+							,'def'=>array(
+								'pattern'=>array(
+									'ABC'=>array('action'=>'abc.def.Ghi')
+								)
+							)
+						)
+					));
+			if(eq(true,isset($map['pattern']['abc']))){
+				eq(true,array_key_exists('name', $map['pattern']['abc']));
+			}
+			if(eq(true,isset($map['pattern']['def/ABC']))){
+				eq(true,array_key_exists('name', $map['pattern']['def/ABC']));
+			}
+		 */
 	}
 }
