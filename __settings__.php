c81eed52c9d7802e56a2f25e86e3ee1b0f481359
tokushima <rhaco1004@gmail.com>
2013-02-06 11:27:59 +0900

    libパスはset_include_pathでやる

--- __settings__.php
+++ __settings__.php
@@ -1,5 +1,6 @@
 <?php
 
+define('PHPMAN_APPENV','local');
 
 \phpman\Conf::set('phpman.Dao','connection',array(
 		'*'=>array()
@@ -11,4 +12,3 @@
 
 \phpman\Conf::set('local.log.OneFile','path',__DIR__.'/work/output.log');
 //\phpman\Log::set_module(new \local\log\OneFile());
-
--- bootstrap.php
+++ bootstrap.php
@@ -21,10 +21,6 @@ spl_autoload_register(function($c){
 	return false;
 },true,false);
 
-if(!defined('PHPMAN_COMMONDIR') && is_dir(__DIR__.'/commons')) define('PHPMAN_COMMONDIR',__DIR__.'/commons');
-if(!defined('PHPMAN_LIBDIR') && is_dir(__DIR__.'/lib')) define('PHPMAN_LIBDIR',__DIR__.'/lib');
-if(defined('PHPMAN_LIBDIR')) set_include_path(constant('PHPMAN_LIBDIR').PATH_SEPARATOR.get_include_path());
-if(defined('PHPMAN_APPENV') && defined('PHPMAN_COMMONDIR') && is_file($f=(defined('PHPMAN_COMMONDIR').'/'.defined('PHPMAN_APPENV').'.php'))) include_once($f);
 ini_set('display_errors','On');
 ini_set('html_errors','Off');
 set_error_handler(function($n,$s,$f,$l){
@@ -35,4 +31,7 @@ if(extension_loaded('mbstring')){
 	if('neutral' == mb_language()) mb_language('Japanese');
 	mb_internal_encoding('UTF-8');
 }
+if(is_dir(__DIR__.'/lib') && strpos(get_include_path(),__DIR__.'/lib') === false) set_include_path(__DIR__.'/lib'.PATH_SEPARATOR.get_include_path());
 if(is_file(__DIR__.'/__settings__.php')) include_once(__DIR__.'/__settings__.php');
+if(!defined('PHPMAN_COMMONDIR') && is_dir(__DIR__.'/commons')) define('PHPMAN_COMMONDIR',__DIR__.'/commons');
+if(defined('PHPMAN_APPENV') && defined('PHPMAN_COMMONDIR') && is_file($f=(constant('PHPMAN_COMMONDIR').'/'.constant('PHPMAN_APPENV').'.php'))) include_once($f);
