b1cf64f7bd5e13f24630fa75fbe351153cc3ed5e
tokushima <rhaco1004@gmail.com>
2013-03-01 00:57:28 +0900

    メモリは少ない方がいい

--- lib/phpman/Plugin.php
+++ lib/phpman/Plugin.php
@@ -7,52 +7,79 @@ trait Plugin{
 	
 	static public function set_class_plugin($o,$n=null){
 		$g = get_called_class();
-		if(is_string($o) && class_exists(($c='\\'.str_replace('.','\\',$o)))) $o = new $c();
-		if(is_string($n)) $n = [$n];
-		
+		if(is_string($o) && class_exists(($c='\\'.str_replace('.','\\',$o)))) $o = new $c();		
 		$t = (is_object($o) ? 1 : 0) + (is_callable($o) ? 2 : 0);
 		if($t === 1){
-			if(!isset($n)) $n = get_class_methods($o);
-			foreach(get_class_methods($o) as $k){
-				if(in_array($k,$n)) self::$_plug_funcs[$g][$k][] = [$o,$k];
-			}
+			self::$_plug_funcs[$g][] = $o;
 		}else if($t === 3 && !empty($n)){
-			foreach($n as $k) self::$_plug_funcs[$g][$k][] = $o;
+			self::$_plug_funcs[$g][] = [$o,(string)$n];
 		}
 	}
 	public function set_object_plugin($o,$n=null){
-		if(is_string($o) && class_exists(($c='\\'.str_replace('.','\\',$o)))) $o = new $c();
-		if(is_string($n)) $n = [$n];
-	
+		if(is_string($o) && class_exists(($c='\\'.str_replace('.','\\',$o)))) $o = new $c();	
 		$t = (is_object($o) ? 1 : 0) + (is_callable($o) ? 2 : 0);
 		if($t === 1){
-			if(!isset($n)) $n = get_class_methods($o);
-			foreach(get_class_methods($o) as $k){
-				if(in_array($k,$n)) $this->_obj_plug_funcs[$g][$k][] = [$o,$k];
-			}
+			$this->_obj_plug_funcs[$g][] = $o;
 		}else if($t === 3 && !empty($n)){
-			foreach($n as $k) $this->_obj_plug_funcs[$g][$k][] = $o;
+			$this->_obj_plug_funcs[$g][] = [$o,(string)$n];
 		}
 	}
 	static protected function has_class_plugin($n){
 		$g = get_called_class();
-		return isset(self::$_plug_funcs[$g][$n]);
-	}
-	static protected function get_class_plugin_funcs($n){
-		$g = get_called_class();
-		return (isset(self::$_plug_funcs[$g][$n])) ? self::$_plug_funcs[$g][$n] : array();
+		if(isset(self::$_plug_funcs[$g])){
+			foreach(self::$_plug_funcs[$g] as $o){
+				if(is_array($o)){
+					if($n == $o[1]) return true;
+				}else if(method_exists($o,$n)){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	protected function has_object_plugin($n){
+		if(static::has_class_plugin($n)) return true;
+
 		$g = get_class($this);
-		if(isset(self::$_plug_funcs[$g][$n])) return true;
-		return isset($this->_obj_plug_funcs[$g][$n]);
+		if(isset($this->_obj_plug_funcs[$g])){
+			foreach($this->_obj_plug_funcs[$g] as $o){
+				if(is_array($o)){
+					if($n == $o[1]) return true;
+				}else if(method_exists($o,$n)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	static protected function get_class_plugin_funcs($n){
+		$rtn = [];
+		$g = get_called_class();
+		if(isset(self::$_plug_funcs[$g])){
+			foreach(self::$_plug_funcs[$g] as $o){
+				if(is_array($o)){
+					if($n == $o[1]) $rtn[] = $o;
+				}else if(method_exists($o,$n)){
+					$rtn[] = [$o,$n];
+				}
+			}
+		}
+		return $rtn;
 	}
 	protected function get_object_plugin_funcs($n){
+		$rtn = static::get_class_plugin_funcs($n);
 		$g = get_class($this);
-		return array_merge(
-					((isset(self::$_plug_funcs[$g][$n])) ? self::$_plug_funcs[$g][$n] : array())
-					,((isset($this->_obj_plug_funcs[$g][$n])) ? $this->_obj_plug_funcs[$g][$n] : array())
-				);
+		if(isset($this->_obj_plug_funcs[$g])){
+			foreach($this->_obj_plug_funcs[$g] as $o){
+				if(is_array($o)){
+					if($n == $o[1]) $rtn[] = $o;
+				}else if(method_exists($o,$n)){
+					$rtn[] = [$o,$n];
+				}
+			}
+		}
+		return $rtn;
 	}
 	static protected function call_class_plugin_funcs($n){
 		$r = null;
@@ -65,7 +92,6 @@ trait Plugin{
 		$r = null;
 		$a = func_get_args();
 		array_shift($a);
-		foreach(static::get_class_plugin_funcs($n) as $o) $r = call_user_func_array($o,$a);
 		foreach($this->get_object_plugin_funcs($n) as $o) $r = call_user_func_array($o,$a);
 		return $r;
 	}
