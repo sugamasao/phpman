84797d72cc844ff9d443e178043c5beeb00d364e
tokushima <rhaco1004@gmail.com>
2013-03-14 08:27:11 +0900

    allinone

---
+++ test/phpman/Template/allinone.php
@@ -0,0 +1,23 @@
+<?php
+$template = new \phpman\Template();
+$src = $template->read(__DIR__.'/resources/allinone.html','abc');
+eq('ABC',$src);
+eq('IFOOTER',$src);
+
+$template = new \phpman\Template();
+$src = $template->read(__DIR__.'/resources/allinone.html','def');
+eq('DEF',$src);
+eq('DFOOTER',$src);
+
+$template = new \phpman\Template();
+$src = $template->read(__DIR__.'/resources/allinone.html','xyz');
+eq('XYZ',$src);
+eq('IFOOTER',$src);
+
+$template = new \phpman\Template();
+$src = $template->read(__DIR__.'/resources/allinone.html','index');
+eq('INDEX',$src);
+eq('IFOOTER',$src);
+
+
+
